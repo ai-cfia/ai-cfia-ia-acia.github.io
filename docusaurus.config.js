b4253b66abffcf34e19126705a2e60371c42a620
reda <tarzaltreda@gmail.com>
2023-11-14 16:48:09 -0500

    issue #42: update baseURL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://ai-cfia-ia-acia.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'ai-cfia-ia-acia.github.io/',
+  baseUrl: '/ai-cfia-ia-acia.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
