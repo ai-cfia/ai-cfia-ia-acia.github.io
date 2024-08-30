a82a2a4dbf1b22ff9aedad8f9259f47153902fdf
ClemZ31 <Clement.Deffes@inspection.gc.ca>
2024-08-30 14:27:08 -0400

    Update fetching logic to support multiple languages

--- src/pages/dev-rel-docs-fetched.js
+++ src/pages/dev-rel-docs-fetched.js
@@ -1,15 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
-// imported from : https://github.com/remarkjs/react-markdown
-
 import Layout from '@theme/Layout';
-
 
 const DynamicContent = () => {
   const [content, setContent] = useState('');
 
   useEffect(() => {
-    fetch('https://raw.githubusercontent.com/ai-cfia/dev-rel-docs/main/README.md')
+    const lang = document.documentElement.lang || 'en';
+
+    let fetchUrl;
+    if (lang === 'fr') {
+      fetchUrl = 'https://raw.githubusercontent.com/ai-cfia/dev-rel-docs/main/README.fr-ca.md';
+    } else {
+      fetchUrl = 'https://raw.githubusercontent.com/ai-cfia/dev-rel-docs/main/README.md';
+    }
+
+    fetch(fetchUrl)
       .then(response => {
         if (!response.ok) {
           throw new Error(`Status: ${response.status}`);
@@ -20,7 +26,6 @@ const DynamicContent = () => {
         setContent(data);
       })
       .catch(() => {
-        // If there is an error fetching the data, redirect to the static page
         window.location.href = 'docs/dev-rel-docs';
       });
   }, []);
@@ -30,26 +35,26 @@ const DynamicContent = () => {
       title="Dev-Rel-Docs-Fetched"
       description="Fetches the content of the README.md file from the dev-rel-docs repository."
     >
-
-        <div class="docPage__5DB">
-          <main class="docMainContainer_gTbr docMainContainerEnhanced_Uz_u">
-            <div class="container padding-top--md padding-bottom--lg">
-              <div class="row">
-                <div class="col docItemCol_VOVn">
-                  <div class="docItemContainer_Djhp">
+      <div className="docsWrapper_hBAB">
+        <div className="docRoot_UBD9">
+          <main className="docMainContainer_TBSr docMainContainerEnhanced_lQrH">
+            <div className="container padding-top--md padding-bottom--lg">
+              <div className="row">
+                <div className="col docItemCol_VOVn">
+                  <div className="docItemContainer_Djhp">
                     <article>
-                      <div class="theme-doc-markdown markdown">
+                      <div className="theme-doc-markdown markdown">
                         <ReactMarkdown>{content}</ReactMarkdown>
                       </div>
                     </article>
+                    <nav className="pagination-nav docusaurus-mt-lg" aria-label="Docs pages"></nav>
                   </div>
                 </div>
               </div>
             </div>
           </main>
         </div>
-
-
+      </div>
     </Layout>
   );
 };
